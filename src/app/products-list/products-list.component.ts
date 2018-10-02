1e5c4d97c563f43a9714089558ce9857f003d45a
Ivy Mutiso <ivy.mwonge@gmail.com>
2018-10-02 09:02:01 +0300

    UI clean up

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -4,7 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
 
